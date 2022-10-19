3eee0b4ce9f2be62dab6c28a72a91028567e8e33
Sheantoo <pirat20013@gmail.com>
2022-10-19 21:20:48 +0300

    init

---
+++ vitrina/app.js
@@ -0,0 +1,68 @@
+const products = [
+    {
+        id: 1,
+        title: 'Lenovo Yoga',
+        price: 3000,
+    },
+    {
+        id: 2,
+        title: 'Acer Aspire',
+        price: 1800,
+    },
+    {
+        id: 3,
+        title: 'Dell Vostro',
+        price: 3400
+    },
+];
+
+let order = [];
+
+function addToBasket(productId) {
+    // TODO: добавить проверку наличия товара в заказе (при наличии выдать alert, что товар уже в корзине)
+    if(order.find(item =>item.id === productId)){
+        alert('Товар уже в корзине');
+    }else{
+        order.push(products.find(item =>item.id ===productId));
+    }
+
+    // TODO: если товар еще не в корзине, добавить его из массива products
+
+    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+    renderCart();
+    rerenderTotalPrice();
+}
+
+function removeFromBasket(productId) {
+    // TODO: описать логику удаления товара из корзины
+    if(order.find(item => item.id === productId)){
+        order.splice(order.findIndex(item => item.id === productId), 1)
+    }else{
+        alert('Этого товара в корзине нет')
+    }
+    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+    renderCart();
+    rerenderTotalPrice();
+}
+
+
+function rerenderTotalPrice() {
+    // TODO: опишите функционал подсчета общей стоимости заказа
+    let totalPrice = 0;
+    order.map(item => totalPrice += item.price);
+    // Не меняйте эту строчку
+    document.getElementById('total').innerText = totalPrice;
+}
+
+// Этот метод остается без изменений
+function renderCart() {
+    const cart = document.getElementById('basket-items');
+
+    cart.innerHTML = '';
+    order.forEach(item => {
+        const el = document.createElement('li');
+        el.innerText = item.title;
+        el.onclick = () => removeFromBasket(item.id);
+        cart.appendChild(el);
+    })
+}
